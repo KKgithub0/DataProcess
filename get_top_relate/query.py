530fab44d799e9186a93d35431598cc138cc6ed2
Snowman <xukai_bx@163.com>
2017-07-24 17:36:18 +0800

    Create query.py

---
+++ get_top_relate/query.py
@@ -0,0 +1,56 @@
+#coding=utf-8
+import sys
+
+def get_top_query():
+    userid_dic = dict()
+    query_dic = dict()
+    last_uid = ''
+    top_num = 10
+    for line in sys.stdin:
+        arr = line.strip().decode('gbk', 'ignore').encode('utf-8', 'ignore').split('\001')
+        if len(arr) != 5:
+            continue
+        uid = arr[0]
+        if last_uid != uid and last_uid != '':
+            sorted_dic = sorted(query_dic.items(), key=lambda d:d[1], reverse = True)
+            count = 0
+            try:
+                output = last_uid + '\t' + uid_company[last_uid]
+            except:
+                output = last_uid + '\t' + 'unknown'
+            for key, value in sorted_dic:
+                if count < top_num:
+                    output += '\t' + key
+                    count += 1
+                else:
+                    break
+            print output
+            last_uid = ''
+            query_dic.clear()
+        last_uid = uid
+        query_dic[arr[1]] = float(arr[-1])
+
+    sorted_dic = sorted(query_dic.items(), key=lambda d:d[1], reverse = True)
+    count = 0
+    output = last_uid + '\t' + uid_company[last_uid]
+    for key, value in sorted_dic:
+        if count < top_num:
+            output += '\t' + key
+            count += 1
+        else:
+            break
+    print output
+
+def get_company_trade():
+    userid_company = dict()
+    with open('./uid_trade_company.csv', 'r') as f:
+        for line in f:
+            arr = line.strip().decode('gbk', 'ignore').encode('utf-8', 'ignore').split(';')
+            if len(arr) != 5:
+                continue
+            userid_company[arr[0]] = arr[1] + '\t' + arr[-1]
+
+    return userid_company
+if __name__ == '__main__':
+    uid_company = get_company_trade()
+    get_top_query()

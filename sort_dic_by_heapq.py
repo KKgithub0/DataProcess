106f43f3dce2c4cb6f1af0984ef7fc63518d9d64
Snowman <xukai_bx@163.com>
2017-08-09 16:13:30 +0800

    Update sort_dic_by_heapq.py

--- sort_dic_by_heapq.py
+++ sort_dic_by_heapq.py
@@ -1,6 +1,7 @@
 import sys
 import heapq
 import Queue
+#get test dictionary
 def get_dic(file):
     dic = dict()
     with open(file, 'r') as f:
@@ -11,6 +12,7 @@ def get_dic(file):
             dic[arr[0]] = arr[1]
 
     return dic
+
 #insert dic element as tuple, so we can sort them as value
 def sort_dic_by_heapq():
     heap = []
@@ -22,6 +24,7 @@ def sort_dic_by_heapq():
     for item in list:
         print str(item[1]) + '\t' + str(item[0])
         
+#using PriorityQueue        
 def sort_dic_by_priority_queue():
     queue = Queue.PriorityQueue()
     for k, v in dic.iteritems():
@@ -30,7 +33,54 @@ def sort_dic_by_priority_queue():
     while count < 10 and not queue.empty():
         print queue.get()
         
-#we can also use class defined by yourself to redefine the compare function
-#detail and reference: http://blog.csdn.net/liu2012huan/article/details/53264162
+#using K to limit the size of heap   
+#to get topK of smallest by restore the value by -value
+#get topK of largest
+def sort_dic_by_heapq_topK():
+    heap = []
+    dic = get_dic('./word_pv.txt')
+    # put all element into heapq or use count to limit its size
+    for k, v in dic.iteritems():
+        if len(heap) <= 10:
+            heapq.heappush(heap, (int(v), k))
+        else:
+            if (int(v) > heap[0][0]):
+                heapq.heapreplace(heap, (int(v), k))
+    for item in heap:
+        print str(item[1]) + '\t' + str(item[0])  
+        
+#we can also use class defined by yourself to define the compare function
+#reference: http://blog.csdn.net/liu2012huan/article/details/53264162
+#looking for source code of heapq
+#def cmp_lt(x, y):
+# Use __lt__ if available; otherwise, try __le__.
+# In Py3.x, only __lt__ will be called.
+#    return (x < y) if hasattr(x, '__lt__') else (not y <= x)
+class Skill(object):
+    def __init__(self,priority,description):
+        self.priority = priority
+        self.description = description
+    def __lt__(self,other):#operator < 
+        return self.priority < other.priority
+    def __ge__(self,other):#oprator >=
+        return self.priority >= other.priority
+    def __le__(self,other):#oprator <=
+        return self.priority <= other.priority
+    def __cmp__(self,other):
+    #call global(builtin) function cmp for int
+        return cmp(self.priority,other.priority)
+    def __str__(self):
+        return '(' + str(self.priority)+',\'' + self.description + '\')'
+
+def heapq_class():
+    heap  = []
+    heapq.heappush(heap,Skill(5,'proficient'))
+    heapq.heappush(heap,Skill(10,'expert'))
+    heapq.heappush(heap,Skill(1,'novice'))
+    while heap:
+        print heapq.heappop(heap),
+    print 
+    
+#the same use of prioriry queue
 if __name__ == '__main__':
     sort_dic_by_heapq()
